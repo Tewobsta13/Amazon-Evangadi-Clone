353c5860c647beebba05a171eb48c592d7adf218
Tewobsta13 <tewobstal@gmail.com>
2025-12-24 18:25:20 +0000

    Clear gh-pages for fresh deploy

--- src/Components/CurrencyFormat/CurrencyFormat.jsx
+++
@@ -1,8 +0,0 @@
-import numeral from "numeral";
-
-const CurrencyFormat = ({ amount }) => {
-  const formattedAmount = numeral(amount).format("$0,0.00");
-  return <span>{formattedAmount}</span>;
-};
-
-export default CurrencyFormat;
--- src/api/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "https://amazon-api-deploy-3a2c.onrender.com",
-});
-
-export { axiosInstance };
